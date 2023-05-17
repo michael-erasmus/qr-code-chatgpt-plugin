afb52a7e046db156384471829915ba6019f6a8fb
Michael Erasmus <hi@michaelerasm.us>
2023-05-17 08:22:04 -0700

    Initial commit, dev version working

--- main.py
+++ main.py
@@ -2,37 +2,26 @@ import json
 
 import quart
 import quart_cors
-from quart import request
+from quart import request, jsonify
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
-# Keep track of todo's. Does not persist if Python session is restarted.
-_TODOS = {}
+@app.route('/create-qr-code', methods=['GET'])
+async def create_qr_code():
+    data = request.args.get('data', '')
+    size = request.args.get('size', '100x100')
+    alt = request.args.get('alt', '')
+    title = request.args.get('title', '')
 
-@app.post("/todos/<string:username>")
-async def add_todo(username):
-    request = await quart.request.get_json(force=True)
-    if username not in _TODOS:
-        _TODOS[username] = []
-    _TODOS[username].append(request["todo"])
-    return quart.Response(response='OK', status=200)
+    img_src = f"https://api.qrserver.com/v1/create-qr-code/?data={data}&size={size}"
 
-@app.get("/todos/<string:username>")
-async def get_todos(username):
-    return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
+    img_tag = f'<img src="{img_src}" alt="{alt}" title="{title}" />'
 
-@app.delete("/todos/<string:username>")
-async def delete_todo(username):
-    request = await quart.request.get_json(force=True)
-    todo_idx = request["todo_idx"]
-    # fail silently, it's a simple plugin
-    if 0 <= todo_idx < len(_TODOS[username]):
-        _TODOS[username].pop(todo_idx)
-    return quart.Response(response='OK', status=200)
+    return jsonify({'img_tag': img_tag})
 
-@app.get("/logo.png")
+@app.get("/qr-logo.png")
 async def plugin_logo():
-    filename = 'logo.png'
+    filename = 'qr-logo.png'
     return await quart.send_file(filename, mimetype='image/png')
 
 @app.get("/.well-known/ai-plugin.json")
